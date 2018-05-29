e8a4514940d8b81493053eea6e4079a2890fdce8
Grégory <gregory.bouloc@gmail.com>
2018-05-29 12:08:25 +0200

    add icom for menu and logo

--- Rmi-session2/src/app/app.component.ts
+++ Rmi-session2/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { faChartBar } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'header';
+  faChartBar = faChartBar;
 }
--- Rmi-session2/src/app/app.module.ts
+++ Rmi-session2/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChartBar } from '@fortawesome/free-solid-svg-icons';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faChartBar);
+library.add(faCog);
+library.add(faSignOutAlt);
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
