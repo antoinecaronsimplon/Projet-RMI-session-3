0a2a09380f28f491eae8e0033fd243fa22e7d7ac
fredsalm <salmsimplon@gmail.com>
2018-05-29 15:59:46 +0200

    add arry wif bootstrap

--- Rmi-session2/src/app/app.module.ts
+++ Rmi-session2/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArrayComponent } from './array/array.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArrayComponent
   ],
   imports: [
     BrowserModule
