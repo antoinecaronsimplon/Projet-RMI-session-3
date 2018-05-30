54a06878da87a3b4085e68ab837f364948e2a0b1
Grégory <gregory.bouloc@gmail.com>
2018-05-30 16:39:51 +0200

    update css header

--- Rmi-session2/src/app/app.module.ts
+++ Rmi-session2/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ArrayComponent } from './array/array.component';
+import { CommandeComponent } from './commande/commande.component';
+import { AdminComponent } from './admin/admin.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 // Fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -14,13 +17,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faChartBar } from '@fortawesome/free-solid-svg-icons';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { CommandeComponent } from './commande/commande.component';
-import { AdminComponent } from './admin/admin.component';
-import { ConnexionComponent } from './connexion/connexion.component';
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
 
 library.add(faChartBar);
 library.add(faCog);
 library.add(faSignOutAlt);
+library.add(faFolderOpen);
+library.add(faHome);
 
 @NgModule({
   declarations: [
