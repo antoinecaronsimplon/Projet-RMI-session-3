bb4ba9db76062d0efd7c4be3dd4799b41f4e15a0
Grégory <gregory.bouloc@gmail.com>
2018-05-30 11:58:44 +0200

    add routing module

---
+++ Rmi-session2/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ArrayComponent } from '../array/array.component';
+import { AdminComponent} from '../admin/admin.component';
+
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'array', component: ArrayComponent },
+  
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    [RouterModule.forRoot(routes)]
+  ],exports: [
+    RouterModule
+],
+  declarations: []
+})
+export class AppRoutingModule { }
