d06e96a1024ddaef87d70c79b111690ebed928b7
Grégory <gregory.bouloc@gmail.com>
2018-06-04 16:16:49 +0200

    ahh

--- Rmi-session2/src/app/commande/commande.component.ts
+++ Rmi-session2/src/app/commande/commande.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClient} from '@angular/common/http';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-commande',
