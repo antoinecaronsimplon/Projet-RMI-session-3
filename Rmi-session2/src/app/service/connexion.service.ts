bc10d001fcfa82d2d8abd65e87c8fd47cc4b612c
antoinecaronsimplon <antoinecaron.simplon@gmail.com>
2018-06-06 14:56:45 +0200

    recup

---
+++ Rmi-session2/src/app/service/connexion.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IDataUser } from '../../assets/data/model/model.user';
+import { Observable, of } from 'rxjs';
+
+
+@Injectable()
+
+export class DataService {
+    private _url: string = '../assets/data/data_test/user.data.json'; // @TODO: tranform environnement
+
+    constructor(private http: HttpClient){}
+
+    getData(): Observable<IDataUser[]>{
+        return this.http.get<IDataUser[]>(this._url);
+    }
+
+
+}
