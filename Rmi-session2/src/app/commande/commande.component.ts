1472a4ec55ed14c2640c20a42200451a66dc9de7
Grégory <gregory.bouloc@gmail.com>
2018-06-08 12:40:39 +0200

    add link with data on admistration page + page commande

--- Rmi-session2/src/app/commande/commande.component.ts
+++ Rmi-session2/src/app/commande/commande.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { HttpClient} from '@angular/common/http';
 import { ListService } from '../service/list.service';
+import { ConnexionService } from '../service/connexion.service';
 
 @Component({
   selector: 'app-commande',
@@ -9,10 +10,15 @@ import { ListService } from '../service/list.service';
   styleUrls: ['./commande.component.css']
 })
 export class CommandeComponent implements OnInit {
-
-  constructor() { }
+  public liste_commandes = [];
+  public liste_user = [];
+  constructor( private editCommande: ListService, private user:ConnexionService) {}
 
   ngOnInit() {
+    this.editCommande.getData()
+        .subscribe(data => { console.log(this.liste_commandes = data )});  
+    this.user.getData()
+        .subscribe(data => {console.log(this.liste_user = data)});
   }
 
 }
